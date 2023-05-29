e085aa8666a007802cd10a0098cf79a437dca540
Atlinx <atlinuxtm@gmail.com>
2023-05-29 21:41:38 +0000

    Update release to output generated at f344157

---
+++ HintString.cs
@@ -0,0 +1,37 @@
+﻿using Godot;
+
+namespace Fractural
+{
+    public static partial class HintString
+    {
+        public enum DictNodeVarsMode
+        {
+            /// <summary>
+            /// DictNodeVars are customizable on the Node
+            /// </summary>
+            Local,
+            /// <summary>
+            /// Fixed DictNodeVars will be generated for properties with the DictNodeVar attribute.
+            /// </summary>
+            Attributes,
+            /// <summary>
+            /// DictNodeVars are customizable on the Node, and fixed DictNodeVars will be generated for properties with the DictNodeVar attribute.
+            /// </summary>
+            LocalAttributes,
+        }
+
+        public static string DictNodeVars(DictNodeVarsMode mode = DictNodeVarsMode.LocalAttributes)
+        {
+            return $"{nameof(DictNodeVars)},{mode}";
+        }
+
+        public static void AddDictNodeVarsProp(this PropertyListBuilder builder, string name, DictNodeVarsMode mode = DictNodeVarsMode.LocalAttributes)
+        {
+            builder.AddItem(
+                name: name,
+                type: Variant.Type.Dictionary,
+                hintString: DictNodeVars(mode)
+            );
+        }
+    }
+}
---
+++ NodeVarsPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Fractural.Plugin;
+using Godot;
+
+#if TOOLS
+namespace Fractural.NodeVars
+{
+    [Tool]
+    public class NodeVarsPlugin : ExtendedPlugin
+    {
+        public override string PluginName => "Fractural Node Vars";
+
+        protected override void Load()
+        {
+            AddManagedInspectorPlugin(new DictNodeVarsInspectorPlugin(this));
+        }
+    }
+}
+#endif

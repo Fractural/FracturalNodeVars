01e4f27a0a452a9c979b702b0af23a6f819ab98a
Atlinx <atlinuxtm@gmail.com>
2023-06-28 01:46:53 +0000

    Update release to output generated at 3ce0acd

--- NodeVarContainer/NodeVarContainer/NodeVarsContainerExtensions.cs
+++ NodeVarContainer/NodeVarContainer/NodeVarsContainerExtensions.cs
@@ -2,6 +2,22 @@
 {
     public static class NodeVarsContainerExtensions
     {
-        public static T GetNodeVar<T>(this INodeVarContainer container, string key) => (T)container.GetNodeVar(key);
+        public static void SetNodeVar(this INodeVarContainer container, string key, object value, bool includePrivate)
+        {
+            if (includePrivate && container is IPrivateNodeVarContainer privateContainer)
+                privateContainer.PrivateSetNodeVar(key, value);
+            else
+                container.SetNodeVar(key, value);
+        }
+
+        public static T GetNodeVar<T>(this INodeVarContainer container, string key, bool includePrivate = true)
+        {
+            if (includePrivate && container is IPrivateNodeVarContainer privateContainer)
+                return (T)privateContainer.PrivateGetNodeVar(key);
+            return (T)container.GetNodeVar(key);
+        }
+
+
+        public static T PrivateGetNodeVar<T>(this IPrivateNodeVarContainer container, string key) => (T)container.PrivateGetNodeVar(key);
     }
 }
